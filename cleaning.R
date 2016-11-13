513c53dbcf1ef532e08f1c50af0126cd712a8930
Matt <md3es@virginia.edu>
2016-11-13 13:39:49 -0500

    contact subset

--- cleaning.R
+++ cleaning.R
@@ -4,12 +4,25 @@ library(plyr)
 
 survey <- ScriptDatum
 
-keep <- c('ScriptDatum.ScriptName','ScriptDatum.Response','ScriptDatum.Latitude',
+keep_survey <- c('ScriptDatum.ScriptName','ScriptDatum.Response','ScriptDatum.Latitude',
          'ScriptDatum.Longitude','ScriptDatum.DeviceId','ScriptDatum.Timestamp')
-survey <- survey[keep]
+survey <- survey[keep_survey]
 
 survey <- rename(survey, c('ScriptDatum.ScriptName' = 'question','ScriptDatum.Response' = 'response',
                  'ScriptDatum.Latitude' = 'lat', 'ScriptDatum.Longitude' = 'long',
                  'ScriptDatum.DeviceId' = 'ID','ScriptDatum.Timestamp' = 'Time'))
 
 stress <- subset(survey, question == 'Fatigue and Stress')
+
+contact <- MicrosoftBandContactDatum
+
+keep_contact <- c('MicrosoftBandContactDatum.DeviceId','MicrosoftBandContactDatum.Timestamp',
+                  'MicrosoftBandContactDatum.State')
+
+contact <- contact[keep_contact]
+
+contact <- rename(contact, c('MicrosoftBandContactDatum.DeviceId' = 'ID',
+                             'MicrosoftBandContactDatum.Timestamp' = 'Time',
+                             'MicrosoftBandContactDatum.State' = 'State'))
+
+connected <- subset(contact, State == 2)
