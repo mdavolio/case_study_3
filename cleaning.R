de1c94a6920af6212fd37618d3cd7880f885a822
Matt <md3es@virginia.edu>
2016-11-13 13:30:22 -0500

    survey cleaning

---
+++ cleaning.R
@@ -0,0 +1,13 @@
+# Cleaning script
+
+library(plyr)
+
+survey <- ScriptDatum
+
+keep <- c('ScriptDatum.ScriptName','ScriptDatum.Response','ScriptDatum.Latitude',
+         'ScriptDatum.Longitude','ScriptDatum.DeviceId','ScriptDatum.Timestamp')
+survey <- survey[keep]
+
+survey <- rename(survey, c('ScriptDatum.ScriptName' = 'question','ScriptDatum.Response' = 'response',
+                 'ScriptDatum.Latitude' = 'lat', 'ScriptDatum.Longitude' = 'long',
+                 'ScriptDatum.DeviceId' = 'ID','ScriptDatum.Timestamp' = 'Time'))
