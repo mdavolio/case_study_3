0445b5f7df0e871963723a90832acbae5130b18d
Matt <md3es@virginia.edu>
2016-11-14 11:27:50 -0500

    consolidate window cleaning

---
+++ twenty_four_hours.R
@@ -0,0 +1,97 @@
+# Average desired varaibles over a 24 hour window
+
+# Distance
+for (i in 1:nrow(stress)) {
+  count=0
+  total = 0
+  endtime = as.numeric(stress$Time[i])
+  begintime = as.numeric(stress$Time[i]) - (3600*24)
+  id = stress$ID[i]
+  for(j in 1:nrow(merged_distance)){
+    if(merged_distance$ID[j]==id){
+      if(begintime < as.numeric(merged_distance$Time[j])
+         & as.numeric(merged_distance$Time[j])< endtime)
+        total = total + merged_distance$distance
+      count = count +1
+    }
+  }
+  stress$AverageDistance24[i] = total/count
+}
+
+# GSR
+for (i in 1:nrow(stress)) {
+  count=0
+  total = 0
+  endtime = as.numeric(stress$Time[i])
+  begintime = as.numeric(stress$Time[i]) - (3600*24)
+  id = stress$ID[i]
+  for(j in 1:nrow(merged_gsr)){
+    if(merged_gsr$ID[j]==id){
+      if(begintime < as.numeric(merged_gsr$Time[j])
+         & as.numeric(merged_gsr$Time[j])< endtime)
+        total = total + merged_gsr$Rate
+      count = count +1
+    }
+  }
+  stress$AverageGSR24[i] = total/count
+}
+
+# Total Steps
+for (i in 1:nrow(stress)) {
+  count=0
+  total = 0
+  endtime = as.numeric(stress$Time[i])
+  begintime = as.numeric(stress$Time[i]) - (3600*24)
+  id = stress$ID[i]
+  for(j in 1:nrow(merged_total_steps)){
+    if(merged_total_steps$ID[j]==id){
+      if(begintime < as.numeric(merged_total_steps$Time[j])
+         & as.numeric(merged_total_steps$Time[j])< endtime)
+        total = total + merged_total_steps$TotalSteps
+      count = count +1
+    }
+  }
+  stress$AverageTotalSteps24[i] = total/count
+}
+
+# Steps Ascended
+for (i in 1:nrow(stress)) {
+  sum = 0
+  count=0
+  endtime = as.numeric(stress$Time[i])
+  begintime = as.numeric(stress$Time[i]) - (3600*24)
+  id = stress$ID[i]
+  for(j in 1:nrow(merged_steps_ascended)){
+    if(merged_steps_ascended$ID[j]==id){
+      if(begintime < as.numeric(merged_steps_ascended$Time[j])
+         & as.numeric(merged_steps_ascended$Time[j])< endtime){
+        sum = sum + merged_steps_ascended$Steps[j]
+        count = count + 1
+      }
+      
+    }
+  }
+  
+  stress$steps_ascended_avg_twentyfour[i] = sum / count
+}
+
+# Heart Rate
+for (i in 1:nrow(stress)) {
+  sum = 0
+  count=0
+  endtime = as.numeric(stress$Time[i])
+  begintime = as.numeric(stress$Time[i]) - (3600*24)
+  id = stress$ID[i]
+  for(j in 1:nrow(merged_hr)){
+    if(merged_hr$ID[j]==id){
+      if(begintime < as.numeric(merged_hr$Time[j])
+         & as.numeric(merged_hr$Time[j])< endtime){
+        sum = sum + merged_hr$Rate[j]
+        count = count + 1
+      }
+      
+    }
+  }
+  
+  stress$hr_avg_twentyfour[i] = sum / count
+}
